07667a2c3786521dbc9a3fca9b7b659c495bd73d
Jerry Gu <jerrysonggu@gmail.com>
2025-06-24 17:17:31 +0900

    organized files and started work on hp

---
+++ Assets/Main/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Main.Scripts
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] 
+        private Button startHostButton;
+        [SerializeField] 
+        private Button startClientButton;
+        [SerializeField]
+        private Text healthText;
+        private void Awake()
+        {
+            if (FindAnyObjectByType<EventSystem>()) return;
+            Type inputType = typeof(StandaloneInputModule);
+            GameObject eventSystem = new GameObject("EventSystem", typeof(EventSystem), inputType);
+            eventSystem.transform.SetParent(transform);
+        }
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            startHostButton.onClick.AddListener(StartHost);
+            startClientButton.onClick.AddListener(StartClient);
+        }
+        
+        private void StartClient()
+        {
+            NetworkManager.Singleton.StartClient();
+            DeactivateButtons();
+        }
+
+        private void StartHost()
+        {
+            NetworkManager.Singleton.StartHost();
+            DeactivateButtons();
+        }
+
+        private void DeactivateButtons()
+        {
+            startHostButton.interactable = false;
+            startClientButton.interactable = false;
+        }
+    }
+}
