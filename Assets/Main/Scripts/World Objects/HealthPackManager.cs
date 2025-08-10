6b835e35f90b779fe49b3add5d2abd5db7e9f127
Jerry Gu <ncq9fn@virginia.edu>
2025-08-09 21:59:33 -0400

    FEATURE: Healthpacks now float up and down nicely

--- Assets/Main/Scripts/World Objects/HealthPackManager.cs
+++ Assets/Main/Scripts/World Objects/HealthPackManager.cs
@@ -4,16 +4,40 @@ namespace Main.Scripts.World_Objects
 {
     public class HealthPackManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        public int healthValue = 50;
 
-        // Update is called once per frame
-        void Update()
-        {
+        [SerializeField]
+        private Transform healthPackTransform;
         
+        private bool _goingUp = true;
+        private float _initialHeight;
+        private const float MaxHeight = .1f;
+        private const float HoverSpeed = .1f;
+
+        private void Start()
+        {
+            _initialHeight = healthPackTransform.localPosition.y;
+        }
+        private void Update()
+        {
+            if (_goingUp)
+            {
+                Vector3 newPosition = healthPackTransform.localPosition + new Vector3(0, Time.deltaTime * HoverSpeed, 0);
+                healthPackTransform.position = newPosition;
+                if (healthPackTransform.localPosition.y > _initialHeight + MaxHeight)
+                {
+                    _goingUp = false;
+                }
+            }
+            else
+            {
+                Vector3 newPosition = healthPackTransform.localPosition + new Vector3(0, Time.deltaTime * -HoverSpeed, 0);
+                healthPackTransform.position = newPosition;
+                if (healthPackTransform.localPosition.y < _initialHeight)
+                {
+                    _goingUp = true;
+                }
+            }
         }
     }
 }
