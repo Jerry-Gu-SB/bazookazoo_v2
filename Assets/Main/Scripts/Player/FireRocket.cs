12644fde954927bdf7b81dfe52b2451d833c0547
Jerry Gu <ncq9fn@virginia.edu>
2025-08-26 21:43:28 -0400

    FEATURE: Added Bazooka weapon cooldown

--- Assets/Main/Scripts/Player/FireRocket.cs
+++ Assets/Main/Scripts/Player/FireRocket.cs
@@ -6,22 +6,29 @@ namespace Main.Scripts.Player
 {
     public class FireRocket : NetworkBehaviour
     {
-        public GameObject rocketPrefab;
-        public Transform firePoint;
-        
+        [SerializeField]
+        private Transform firePoint;
         [SerializeField]
         private PlayerManager playerManager;
+        [SerializeField]
+        private GameObject rocketPrefab;
+        [SerializeField]
+        private SpriteRenderer bazookaSpriteRenderer;
+        private float _fireRate = .5f;
+        private float _fireTimer = 0;
+        private bool _hasFired = false;
 
         private void Update()
         {
             if (!IsOwner) return;
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && !_hasFired)
             {
+                _hasFired = true;
                 if (playerManager.isDead) return;
                 FireRocketServerRpc(firePoint.position, firePoint.rotation);
             }
-            
+            CalculateFiredTimer();
         }
 
         [ServerRpc]
@@ -33,6 +40,38 @@ namespace Main.Scripts.Player
 
             RocketProjectile rp = rocket.GetComponent<RocketProjectile>();
             rp.shooterNetworkID.Value = this.GetComponent<NetworkObject>().NetworkObjectId;
+        }
+        private void CalculateFiredTimer()
+        {
+            if (_hasFired)
+            {
+                _fireTimer += Time.deltaTime;
+                if  (_fireTimer >= _fireRate)
+                {
+                    _fireTimer = 0;
+                    _hasFired = false;
+                }
+            }
+        }
+
+        public void SetFireRate(float fireRate)
+        {
+            _fireRate = fireRate;
+        }
+
+        public void SetBazookaSprite(Sprite bazookaSprite)
+        {
+            bazookaSpriteRenderer.sprite = bazookaSprite;
+        }
+
+        public float GetFireRate()
+        {
+            return _fireRate;
+        }
+
+        public SpriteRenderer GetBazookaSpriteRenderer()
+        {
+            return bazookaSpriteRenderer;
         }
     }
 }
